519a5b263222f49c9d6150187cc13ecd76f8c4ed
David Brown <davidb@davidb.org>
2016-05-01 21:53:22 +0000

    filer: Decode the 'back' node.

--- filer/src/bin/filer.rs
+++ filer/src/bin/filer.rs
@@ -1,11 +1,16 @@
 // Show a tree.
 
 extern crate cas;
+extern crate byteorder;
 
+use byteorder::{BigEndian, ReadBytesExt};
 use cas::{Kind, Oid};
+use cas::Result;
 use cas::pdump::HexDump;
 use cas::pool::{AdumpPool, ChunkSource};
+use std::collections::BTreeMap;
 use std::env;
+use std::io::Read;
 
 fn main() {
     let mut argsi = env::args();
@@ -45,5 +50,35 @@ impl<'a> Walk<'a> {
         let ch = self.source.find(id).unwrap();
         assert_eq!(ch.kind(), Kind::new("back").unwrap());
         (&ch.data()[..]).dump();
+
+        let mut buf = &ch.data()[..];
+
+        let kind = buf.read_string1().unwrap();
+
+        let mut dict = BTreeMap::new();
+        while buf.len() > 0 {
+            let key = buf.read_string1().unwrap();
+            let value = buf.read_string2().unwrap();
+            dict.insert(key, value);
+        }
+        println!("kind: {:?}, dict: {:#?}", kind, dict);
     }
 }
+
+trait Decode: Read {
+    fn read_string1(&mut self) -> Result<String> {
+        let len = try!(self.read_u8());
+        let mut buf = vec![0u8; len as usize];
+        try!(self.read_exact(&mut buf));
+        Ok(try!(String::from_utf8(buf)))
+    }
+
+    fn read_string2(&mut self) -> Result<String> {
+        let len = try!(self.read_u16::<BigEndian>());
+        let mut buf = vec![0u8; len as usize];
+        try!(self.read_exact(&mut buf));
+        Ok(try!(String::from_utf8(buf)))
+    }
+}
+
+impl<T: Read> Decode for T {}
